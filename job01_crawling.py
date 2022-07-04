8a1d6fef1965651c1de88ebc2aea0260ffaf3276
shine525 <0620julie@gmail.com>
2022-07-04 15:59:50 +0900

    crawling_final

--- job01_crawling.py
+++ job01_crawling.py
@@ -29,7 +29,7 @@ review_xpath = '//*[@id="content"]/div[1]/div[4]/div[1]/div[4]'
 
 #네이버영화랭킹-디렉토리-개봉년도(2020) 페이지
 for i in range(1, 38): # ~38
-    url = 'https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open={}}&page={}'.format(your_year, i)
+    url = 'https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open={}&page={}'.format(your_year, i)
     titles = []
     reviews = []
     try:
@@ -52,23 +52,28 @@ for i in range(1, 38): # ~38
                     try:
                         driver.find_element("xpath", review_page_button_xpath).click()
                         for l in range(1, 11): # ~11
+                            back_flag = False
                             review_title_xpath = '//*[@id="reviewTab"]/div/div/ul/li[{}]/a'.format(l)
                             try:
                                 review = driver.find_element("xpath", review_title_xpath).click()
+                                back_flag = True
                                 time.sleep(0.5)
                                 review = driver.find_element("xpath", review_xpath).text
-                                print(title)
-                                print(review)
+                                titles.append(title)
+                                reviews.append(review)
                                 driver.back()
                             except:
+                                if back_flag :
+                                    driver.back()  #back_flag=True일때, drive.back()
                                 print('review', i, j, k, l)
                         driver.back()
                     except:
                         print('review page', i, j ,k)
-                driver.back()
             except:
                 print('movie', i, j)
+        df = pd.DataFrame({'title':titles, 'reviews':reviews})
+        df.to_csv('./crawling_data/reviews_{}_{}page.csv'.format(your_year, i), index=False)
     except:
         print('page', i)
-    finally:
-        pass
+
+driver.close()
